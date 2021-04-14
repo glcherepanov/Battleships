52f4b87be77d37a70a5b39ad3b4bbb1c3383183a
beatrate <inik4us@gmail.com>
2021-04-14 22:13:54 +0300

    Removed old win text. Added testing utility.

---
+++ Assets/Scripts/DesktopTest.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using Vuforia;
+
+[DefaultExecutionOrder(100)]
+public class DesktopTest : MonoBehaviour
+{
+	private TrackableBehaviour trackableBehaviour = null;
+
+	private void Awake()
+	{
+		if(!Application.isEditor && Application.isMobilePlatform)
+		{
+			Destroy(this);
+			return;
+		}
+
+		trackableBehaviour = GetComponent<TrackableBehaviour>();
+	}
+
+	private void Update()
+	{
+		var rendererComponents = trackableBehaviour.GetComponentsInChildren<Renderer>(true);
+		var colliderComponents = trackableBehaviour.GetComponentsInChildren<Collider>(true);
+		var canvasComponents = trackableBehaviour.GetComponentsInChildren<Canvas>(true);
+
+		// Enable rendering:
+		foreach(var component in rendererComponents)
+			component.enabled = true;
+
+		// Enable colliders:
+		foreach(var component in colliderComponents)
+			component.enabled = true;
+
+		// Enable canvas':
+		foreach(var component in canvasComponents)
+			component.enabled = true;
+	}
+}
