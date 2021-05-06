0cadcf15b876f75247adcc8beb0be0c48b088001
beatrate <inik4us@gmail.com>
2021-05-06 20:23:20 +0300

    Added debug console.

---
+++ Assets/Scripts/DebugLoader.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DebugLoader
+{
+	[RuntimeInitializeOnLoadMethod]
+	public static void Initialize()
+	{
+		GameObject consolePrefab = Resources.Load<GameObject>("IngameDebugConsole");
+		GameObject.Instantiate(consolePrefab);
+	}
+}
