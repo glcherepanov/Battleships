b11fe2d19ac818c119647281fabb8d1e8baca17d
beatrate <inik4us@gmail.com>
2021-05-07 16:20:07 +0300

    Fixed screen orientation.

---
+++ Assets/Scripts/OrientationControl.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class OrientationControl
+{
+	private enum Orientation
+	{
+		Landscape,
+		Portrait
+	}
+
+	private const string LevelScene = "Level";
+
+	[RuntimeInitializeOnLoadMethod]
+	public static void Initialize()
+	{
+		UpdateOrientation(SceneManager.GetActiveScene());
+		SceneManager.sceneLoaded += SceneLoaded;
+	}
+
+	private static void SceneLoaded(Scene scene, LoadSceneMode mode)
+	{
+		UpdateOrientation(scene);
+	}
+
+	private static void UpdateOrientation(Scene scene)
+	{
+		if(scene.name == LevelScene)
+		{
+			ForceOrientation(Orientation.Landscape);
+		}
+		else
+		{
+			ForceOrientation(Orientation.Portrait);
+		}
+	}
+
+	private static void ForceOrientation(Orientation orientation)
+	{
+		switch(orientation)
+		{
+			case Orientation.Landscape:
+				Screen.autorotateToPortrait = false;
+				Screen.autorotateToPortraitUpsideDown = false;
+				Screen.autorotateToLandscapeLeft = true;
+				Screen.autorotateToLandscapeRight = true;
+				Screen.orientation = ScreenOrientation.Landscape;
+				break;
+			case Orientation.Portrait:
+				Screen.autorotateToPortrait = true;
+				Screen.autorotateToPortraitUpsideDown = true;
+				Screen.autorotateToLandscapeLeft = false;
+				Screen.autorotateToLandscapeRight = false;
+				Screen.orientation = ScreenOrientation.Portrait;
+				break;
+		}
+	}
+}
